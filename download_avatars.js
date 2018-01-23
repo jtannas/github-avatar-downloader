4f2a7f4842f82ce2a73478c31c59381ac1a2cf9e
Joel Tannas <jtannas@gmail.com>
2018-01-22 18:53:33 -0700

    implement a downloadImage function

--- download_avatars.js
+++ download_avatars.js
@@ -1,4 +1,5 @@
 const request = require('request');
+const fs = require('fs');
 const apiToken = require('./secrets').GITHUB_API_TOKEN;
 
 console.log('Welcome to the GitHub Avatar Downloader!');
@@ -24,3 +25,20 @@ getRepoContributors("jquery", "jquery", function(err, result) {
     console.log(contributor.avatar_url);
   });
 });
+
+
+const downloadImage = function downloadImageByURL(url, fileName) {
+  request.get(url)
+    .on('error', err => { throw err; })
+    .on('response', response => {
+      console.log(`Response Code: ${response.statusCode} - ${response.statusMessage}`);
+      console.log(`content-type: ${response.headers['content-type']}`);
+      console.log('... Download beginning ...');
+    })
+    .pipe(fs.createWriteStream(`./avatars/${fileName}`)
+      .on('error', err => { throw err; })
+      .on('finish', end => console.log('--- Download Complete ---'))
+    );
+};
+
+downloadImage("https://avatars2.githubusercontent.com/u/2741?v=3&s=466", "kvirani.jpg");
