406c010bdb35fe8b090e675010899c9bf1b67003
Joel Tannas <jtannas@gmail.com>
2018-01-22 18:25:49 -0700

    complete step 6 of the tutorial

--- download_avatars.js
+++ download_avatars.js
@@ -1,9 +1,19 @@
-var request = require('request');
+const request = require('request');
+const apiToken = require('./secrets').GITHUB_API_TOKEN;
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 const getRepoContributors = function getGithubRepoContributors(repoOwner, repoName, cb) {
-  // ...
+  const options = {
+    url: `https://api.github.com/repos/${repoOwner}/${repoName}/contributors`,
+    headers: {
+      'User-Agent': 'request',
+      Authorization: apiToken
+    }
+  };
+  request(options, function(err, res, body) {
+    cb(err, body);
+  });
 };
 
 getRepoContributors("jquery", "jquery", function(err, result) {
